00b9a45c2c04304196492cdeedea2e7a6254a6b6
KandlaguntaVenkataSivaNiranjanReddy <117029570+KandlaguntaVenkataSivaNiranjanReddy@users.noreply.github.com>
2026-07-30 19:31:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,8 +41,7 @@ resource "aws_security_group" "ec2_security_group" {
 }
 
 resource "aws_instance" "Monitoring_server" {
-ami = "ami-0aba19e56f3eaec05"  
-instance_type = "t3.medium"
+ami = data.aws_ssm_parameter.ubuntu_ami.valueinstance_type = "t3.medium"
 security_groups = [aws_security_group.ec2_security_group.name]
 key_name = var.key_name
 tags = {
